a3aa31f485d51ed293d8fc88fee9b5ada25cc746
Miles Cohn <milesselim@icloud.com>
2020-10-26 21:19:12 -0400

    Added stuff to connect to Heroku, hopefully

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { 
   useNewUrlParser: true, 
-  useUnifiedTopology: true 
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 require("./routes/api.js")(app);
